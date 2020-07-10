104c5cd2521810df5ffb4eb128312fc096a13f86
吉崎和真 <f14vn@MacBook-Pro.local>
2020-07-10 18:49:29 +0900

    laravel-crud-api

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\User;
+use App\Article;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,5 +17,22 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::group(['middleware' => ['api']], function() {
-    Route::resource('posts' , 'Api\PostApiController');
+    Route::resource('articles', 'Api\ArticlesController', ['except' => ['create', 'edit']]);
+
+    //記事を投稿する機能
+    Route::post('/article/{id}', function($id){
+        //投稿するユーザーを取得
+        $user = User::where('id', $id)->first();
+
+        //リクエストデータをもとに記事を作成
+        $article = new Article();
+        $article->title = request('title');
+        $article->content = request('content');
+
+        //ユーザーに関連づけて保存
+        $user->articles()->save($article);
+
+        //テストのためtitle, contentのデータをリターン
+        return ['title' => request('title'), 'content' => request('content')];
+    });
 });
